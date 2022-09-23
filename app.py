599ca2c8eb0baf6d336216d54086a50238d04dc0
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-24 00:06:08 +0100

    Add working experimenting sql database

--- app.py
+++ app.py
@@ -1,56 +1,148 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request 
 from flask_cors import CORS
 from werkzeug import exceptions
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from os import environ
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError, EqualTo
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash 
 
 # Load environment variables
 
 load_dotenv()
 database_uri = environ.get('DATABASE_URL')
+secret_key = environ.get('SECRET_KEY')
 
 # Set up the app 
 
 app = Flask(__name__)
 app.config.update(
     SQLALCHEMY_DATABASE_URI=database_uri,
-    SQLACHEMY_TRACK_MODIFICATIONS=environ.get('SQL_ALCHEMY_TRACK_MODIFICATIONS')
+    SQLACHEMY_TRACK_MODIFICATIONS=environ.get('SQL_ALCHEMY_TRACK_MODIFICATIONS'),
+    SECRET_KEY=secret_key
 )
 
 CORS(app)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# Database
+# Login 
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+# Create database
 
 @app.before_first_request
 def create_tables():
-    # Clear it all out
-    db.drop_all()
-
-    # Set it back 
     db.create_all()
 
 # Models 
 
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
 
+    # Check hash with password 
+
+    @property
+    def password_hash(self):
+	    raise AttributeError('password is not a readable attribute!')
+
+    @password_hash.setter
+    def password_hash(self, password):
+	    self.password_hash = generate_password_hash(password)
+
+    def verify_password_hash(self, password):
+	    return check_password_hash(self.password_hash, password)
+
+# Validate if user exists
+
+def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+
+                
+# Form for demonstration
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    email= StringField(validators=[
+        InputRequired(), Length(min=20, max=100)], render_kw={"placeholder": "Email"})
+
+    password = PasswordField(validators=[
+        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
 
 # Routes 
 
 @app.route('/')
 def home():
-    return "Hello!"
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
+    return "<h1>Hello!</h1>"
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/dashboard',  methods=['GET'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+        new_user = Users(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
