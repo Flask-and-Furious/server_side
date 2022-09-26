53b00b19ce062fada2cc714bc5c90efb2cf71693
B9ary01 <bishalrai111@yahoo.com>
2022-09-26 16:29:36 +0100

    test login method

--- test_app.py
+++ test_app.py
@@ -12,3 +12,30 @@ def test_index_route(api):
 def test_home(api):
    resp = api.get('/')
    assert b"Hello!" in resp.data
+
+
+ 
+ 
+@pytest.fixture(scope='module')
+def flask_app():
+   with app.app_context():
+       yield app
+ 
+ 
+@pytest.fixture(scope='module')
+def client(flask_app):
+   app = flask_app
+   ctx = app.test_request_context()
+   ctx.push()
+   app.test_client_class = FlaskClient
+   return app.test_client()
+ 
+ 
+def test_index_page__logged_in(client):
+   with client:
+       client.post('/login', data=dict(username='user', password='test'))
+       res = client.get('/')
+       assert res.status_code == 200
+
+ 
+ 
