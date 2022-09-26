a46e34951f905fd3bce3fc7f56ef8df009c11508
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-26 16:15:50 +0100

    Add more string

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     # Check hash with password 
 
